5d7bf36232cf62a991290beaf725664e5db379f9
yashmanjappa3010-commits <yashmanjappa3010@gmail.com>
2026-01-04 11:30:37 +0530

    Add files via upload

---
+++ A1task1.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("The Addition of the numbers :",a+b)
+print("The subtraction of the numbers:",a-b)
+print("The multiplication of the numbers:",a*b)
+print("The division of the numbers:",round(a/b,2))
---
+++ A1task2.py
@@ -0,0 +1,4 @@
+a=input("Enter your first name:")
+b=input("Enter your last name:")
+
+print("hello,",a,b,'!',"Welcome to the python program!")
