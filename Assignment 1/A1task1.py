972baf32890d5585c5cda0699b1e4dd14bb67637
yashmanjappa3010-commits <yashmanjappa3010@gmail.com>
2026-01-04 11:31:12 +0530

    Update and rename A1task1.py to Assignment 1/A1task1.py

--- Assignment 1/A1task1.py
+++ Assignment 1/A1task1.py
@@ -4,3 +4,4 @@ print("The Addition of the numbers :",a+b)
 print("The subtraction of the numbers:",a-b)
 print("The multiplication of the numbers:",a*b)
 print("The division of the numbers:",round(a/b,2))
+
